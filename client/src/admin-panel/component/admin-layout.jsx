49f8c6308e05aaca7ff3ba28a2d5a8026746023d
hakeem <abdhakeem123abd@gmail.com>
2026-02-25 17:41:52 +0530

    container issue fixed

--- client/src/admin-panel/component/admin-layout.jsx
+++ client/src/admin-panel/component/admin-layout.jsx
@@ -5,9 +5,9 @@ import Header from './header';
 
 const AdminLayout = () => {
   return (
-    <div className="container-fluid mx-auto pr-3">
-      <div className="flex gap-3 lg:gap-4">
-        <div className=" lg:w-[262px]">
+    <div className="container-fluid mx-auto px-3 lg:px-0 lg:pr-3">
+      <div className="flex gap-0 lg:gap-4">
+        <div className="hidden lg:block lg:w-[262px]">
           <Slidebar />
         </div>
         <div className="w-full pt-3">
