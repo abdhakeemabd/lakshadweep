99c1d0f3781b06e0d2e09d6545d810bab4094bef
hakeem <abdhakeem123abd@gmail.com>
2026-03-12 16:22:22 +0530

    header slidbar issue fixed

--- client/src/admin-panel/component/admin-layout.jsx
+++ client/src/admin-panel/component/admin-layout.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Slidebar from './slidebar';
 import Header from './header';
 
 const AdminLayout = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
   return (
     <div className="container-fluid mx-auto px-3 lg:px-0 lg:pr-3">
       <div className="flex gap-0 lg:gap-4">
-        <div className="hidden lg:block fixed left-0 top-0 h-full w-[262px]">
-          <Slidebar />
+        {/* Sidebar */}
+        <div className="lg:fixed lg:left-0 lg:top-0 lg:h-full lg:w-[262px] z-[1001] lg:z-auto">
+          <Slidebar isOpen={isSidebarOpen} setIsOpen={setIsSidebarOpen} />
         </div>
 
+        {/* Content */}
         <div className="flex-1 w-full lg:ml-[262px] px-3 lg:px-4 pt-3">
-          <Header />
+          <Header onToggleSidebar={() => setIsSidebarOpen(!isSidebarOpen)} />
           <Outlet />
         </div>
       </div>
